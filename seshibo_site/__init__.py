cfa313640b1dbc4849e456175d2d4b8547f306bb
victor <viktormotaung@gmail.com>
2025-11-21 06:29:31 +0200

    Install PyMySQL and shim it as MySQLdb; fix __init__

--- seshibo_site/__init__.py
+++ seshibo_site/__init__.py
@@ -1,2 +1,10 @@
+# yourproject/__init__.py
+import pymysql
+
+# make PyMySQL act as MySQLdb for Django
+pymysql.install_as_MySQLdb()
+
+# keep your celery app import
 from .celery import app as celery_app
+
 __all__ = ("celery_app",)
