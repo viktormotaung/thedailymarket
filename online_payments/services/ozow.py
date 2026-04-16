2c08b2d1ee8e520e0c282dd9654ac94351e4b755
Victor Motaung <victormotaung@gmail.com>
2026-04-16 12:11:04 +0200

    update verify ozow hash

--- online_payments/services/ozow.py
+++ online_payments/services/ozow.py
@@ -6,19 +6,17 @@ def generate_ozow_hash(hash_string):
     final_string = (hash_string + settings.OZOW_PRIVATE_KEY.strip()).lower()
     return hashlib.sha512(final_string.encode("utf-8")).hexdigest()
 
-def verify_ozow_hash(post_data: dict) -> bool:
-    received_hash = (post_data.get("HashCheck") or "").lower()
+def verify_ozow_hash(post_data):
+    received_hash = (post_data.get("Hash") or "").lower()
 
     required_fields = [
         "SiteCode",
         "TransactionId",
         "TransactionReference",
-        "BankReference",
         "Amount",
         "Status",
     ]
 
-    # Ensure all required fields exist
     for field in required_fields:
         if field not in post_data:
             print(f"Missing field in Ozow callback: {field}")
@@ -28,17 +26,16 @@ def verify_ozow_hash(post_data: dict) -> bool:
         post_data["SiteCode"]
         + post_data["TransactionId"]
         + post_data["TransactionReference"]
-        + post_data["BankReference"]
         + post_data["Amount"]
         + post_data["Status"]
     )
 
     calculated_hash = generate_ozow_hash(hash_string)
 
-    print("=== OZOW VERIFY DEBUG ===")
+    print("=== OZOW CALLBACK DEBUG ===")
     print("HASH STRING:", hash_string)
     print("CALCULATED:", calculated_hash)
     print("RECEIVED:", received_hash)
-    print("=========================")
+    print("===========================")
 
     return calculated_hash == received_hash
